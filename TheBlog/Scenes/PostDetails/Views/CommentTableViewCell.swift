e26a84a2bccd6a811589107a1c156f6904f63f15
Marcio Garcia <marcio.garcia@zup.com.br>
2020-06-13 12:24:11 -0300

    Sets comment cell not selectable

--- TheBlog/Scenes/PostDetails/Views/CommentTableViewCell.swift
+++ TheBlog/Scenes/PostDetails/Views/CommentTableViewCell.swift
@@ -32,6 +32,7 @@ class CommentTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
         setupViewConfiguration()
     }
 
